2dd4881db3bb8daf86ee17735f12efce5fe981ed
Omer Oreg <omeroreg@gmail.com>
2026-02-05 15:03:32 +0200

    Add service + update settings

--- LAST_Hub/settings.py
+++ LAST_Hub/settings.py
@@ -25,7 +25,11 @@ SECRET_KEY = 'django-insecure-rnib3=gd=kqojf^rf&g9-ch-vnm7xvg#s+fgj%2%07s3s)sa0i
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "localhost",
+    "127.0.0.1",
+    "10.23.1.25"
+]
 
 
 # Application definition
